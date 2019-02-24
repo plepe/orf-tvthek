1788e12d8b66d330cb8fb4e5212afef6e4371222
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2019-02-24 08:55:34 +0100

    Load segment list

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,7 @@
 const ArgumentParser = require('argparse').ArgumentParser
+const request = require('request')
+const DOMParser = require('xmldom').DOMParser
+
 var parser = new ArgumentParser({
   addHelp: true,
   description: 'Download videos from ORF TVThek'
@@ -9,3 +12,26 @@ parser.addArgument('url', {
 })
 
 let args = parser.parseArgs()
+
+request(args.url, (err, response, body) => {
+  let document = new DOMParser().parseFromString(body)
+
+  let segments = []
+  let lis = document.getElementsByTagName('li')
+  for (let i = 0; i < lis.length; i++) {
+    let li = lis[i]
+
+    if (li.hasAttribute('data-jsb')) {
+      let data = JSON.parse(li.getAttribute('data-jsb'))
+      if (data.video) {
+        segments.push(data.video)
+      }
+    }
+  }
+
+  segments.forEach(segment => downloadSegment(segment, () => {}))
+})
+
+function downloadSegment (data, callback) {
+  console.log(data)
+}
